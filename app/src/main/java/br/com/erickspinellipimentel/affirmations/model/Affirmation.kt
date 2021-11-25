bb67fe2063d1d09ba1f1a8fa1b6cb2297f29a018
Pimentel <EPIMENT5@ford.com>
2021-11-25 08:27:42 -0300

    Projeto Affirmations finalizado

--- app/src/main/java/br/com/erickspinellipimentel/affirmations/model/Affirmation.kt
+++ app/src/main/java/br/com/erickspinellipimentel/affirmations/model/Affirmation.kt
@@ -1,6 +1,9 @@
 package br.com.erickspinellipimentel.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 data class Affirmation(
-    val stringResourceId: Int
-) {
-}
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
