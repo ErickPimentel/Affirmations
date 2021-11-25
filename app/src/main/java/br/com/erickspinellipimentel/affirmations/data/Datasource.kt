5a5324fe57ee0282852128a6ab18d9ef30311349
Pimentel <EPIMENT5@ford.com>
2021-11-25 08:10:45 -0300

    Affirmation, Datasource, ItemAdapter (OnCreateViewHolder, onBindViewholder, getItemCount implementados), ItemViewHolder (classe aninhada dentro de ItemAdapter) e list_item.xml criados / activity_main.xml atualizada (utilizando um containerview chamado FrameLayout e dentro a RecyclerView)

---
+++ app/src/main/java/br/com/erickspinellipimentel/affirmations/data/Datasource.kt
@@ -0,0 +1,22 @@
+package br.com.erickspinellipimentel.affirmations.data
+
+import br.com.erickspinellipimentel.affirmations.model.Affirmation
+import br.com.erickspinellipimentel.affirmations.R
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/br/com/erickspinellipimentel/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package br.com.erickspinellipimentel.affirmations.model
+
+data class Affirmation(
+    val stringResourceId: Int
+) {
+}
